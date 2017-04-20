603f8555a69165159db4c13ec08e9ca00f3cebbe
btname <btname@163.com>
2017-04-20 17:13:38 +0800

    无数据

---
+++ src/views/common/notData.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="notData">
+    <div class="box">
+      <i></i>
+      <span>{{txt}}</span>
+    </div>
+  </div>
+
+</template>
+<script>
+
+export default {
+  props:{
+    txt:{
+      type:String,
+      default:'当前没有数据哦~'
+    }
+  },
+  data() {
+    return{
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+<style scoped lang="scss" src="../../assets/styles/_notData.scss"></style>
