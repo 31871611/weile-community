dd955c468ebf89d561b3ccdd244a8a985bac7888
btname <btname@163.com>
2017-04-21 10:12:26 +0800

    我的地址

---
+++ src/views/userAddress/userAddress.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <div class="wrap">
+
+    <article class="main">
+      <div class="mainScroll userAddress">
+        <div class="title">
+          可配送地址
+        </div>
+        <ul>
+          <li>
+            <div><span class="name">姓名</span><span class="tel">130000000000</span></div>
+            <p>地址地址地址地址地址地址地址地址地址地址地址地址地址地址</p>
+            <i></i>
+          </li>
+          <li>
+            <div><span class="name">姓名</span><span class="tel">130000000000</span></div>
+            <p>地址地址地址地址地址地址地址地址地址地址地址地址地址地址</p>
+            <i></i>
+          </li>
+        </ul>
+        <div class="title">
+          不可配送地址
+        </div>
+        <ul>
+          <li class="select">
+            <div><span class="name">姓名</span><span class="tel">130000000000</span></div>
+            <p>地址地址地址地址地址地址地址地址地址地址地址地址地址地址</p>
+            <i></i>
+          </li>
+        </ul>
+      </div>
+    </article>
+    <footer>
+      <div class="userAddressBtn">
+        <div>提交</div>
+      </div>
+    </footer>
+  </div>
+
+</template>
+<script>
+
+export default {
+  name: 'userAddress',
+  data() {
+    return{
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+<style scoped lang="scss" src="../../assets/styles/userAddress.scss"></style>
