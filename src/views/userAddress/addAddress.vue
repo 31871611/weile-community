649b1ea5f16d5fce6bcc55645b9e13921734667c
btname <btname@163.com>
2017-04-21 17:20:47 +0800

    用户地址

--- src/views/userAddress/addAddress.vue
+++ src/views/userAddress/addAddress.vue
@@ -1,54 +1,57 @@
 <template>
 
-  <div class="wrap">
+  <div class="wrap fullPosition">
 
     <article class="main">
       <div class="mainScroll userAddAddress">
         <div class="import">
-          <input type="text" placeholder="请输入收货人姓名" />
+          <label for="">收货人</label>
+          <input type="text" placeholder="请输入收货人姓名" v-model="name" />
         </div>
         <div class="import">
-          <input type="tel" placeholder="请输入收货人手机号码" />
+          <label for="">手机号</label>
+          <input type="tel" placeholder="请输入收货人手机号码" v-model="mobile" />
         </div>
-        <div class="selectAddress">
-          <span>选择小区</span>
+        <div class="selectAddress" @click="goSelectQuarters">
+          <label for="">小区名称</label>
+          <span>{{distributionCommunityName}}</span>
           <i></i>
         </div>
 
-        <div class="detailedAddress" contenteditable="true">
-          详细地址
+        <div class="detailedAddress">
+          <label for="">详细地址</label>
+          <textarea placeholder="补充详细地址" v-model="address"></textarea>
         </div>
 
-        <div class="setDefault">
-          <label for="setDefault">
-            <span>设为默认地址</span>
-            <input id="setDefault" type="radio" name="setDefault" />
+        <div class="setDefault" @click="setDefault">
+          <label for="">
+            <span>设置此小区为默认地址</span>
+            <i class="radio" :class="{'select':isDefault}"></i>
           </label>
         </div>
 
-        <ul class="userSelectAddress">
-          <li class="select">
-            <label>
-              <i class="map"></i>
-              <span>橘园创意广场</span>
-              <input type="radio" />
-            </label>
-          </li>
-          <li class="select">
-            <label>
-              <i class="map"></i>
-              <span>橘园创意广场</span>
-              <input type="radio" />
-            </label>
-          </li>
-        </ul>
+
+        <transition name="SlideRightLeft">
+          <ul class="userSelectAddress" v-show="isSelectQuarters">
+            <li class="select" v-for="(list,index) in quartersLists">
+              <label @click="selectCurrentQuarters(list.name,list.id)">
+                <i class="map"></i>
+                <span>{{list.name}}</span>
+                <i class="radio" :class="{'select':distributionCommunityId == list.id}"></i>
+              </label>
+            </li>
+          </ul>
+        </transition>
+
+
+        <modal-toast ref="modalToast" :txt="'加载中'" :icon="'loading'" :time="0"></modal-toast>
 
       </div>
     </article>
 
     <footer>
       <div class="userAddAddressBtn">
-        <span>提交</span>
+        <span @click="save()">保存地址</span>
       </div>
     </footer>
 
@@ -56,22 +59,153 @@
 
 </template>
 <script>
+import simplestorage from 'simplestorage.js'
+import modalToast from '../common/modalToast.vue'
 
 export default {
   name: 'userAddAddress',
+  props:['list'],
   data() {
     return{
+      path:this.$route.path,      //当前url，分:新增、修改
+      addressId:'',               //修改地址时使用
+      name:'',                    //必
+      mobile:'',                  //必
+      distributionCommunityName:simplestorage.get('HLXK_DISTRIBUTION').name || '选择小区',
+      distributionCommunityId:simplestorage.get('HLXK_DISTRIBUTION').id || '',
+      address:'',                 //必
+      isDefault:false,            //"1"表示是 “0”不设为默认地址
+      isSelectQuarters:false,     //选择小区弹窗
+      quartersLists:''            //小区列表
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+    let _this = this;
+
+    if(this.path == '/userAddress/modify'){
+      if(!_this.list){
+        _this.$router.back();
+        return false;
+      }
+      // 将数据传递到修改页面
+      let data = _this.list[this.$route.query.index];
+      //console.log(data);
+      _this.distributionCommunityId = this.$route.query.communityId;
+      _this.name = data.name;
+      _this.mobile = data.mobile;
+      _this.address = data.address;
+      _this.isDefault = data.defaultAddress;
 
     }
   },
-  mounted() {
-
-  },
   methods: {
+    // 设置默认
+    setDefault:function(){
+      this.isDefault = !this.isDefault;
+    },
+    // 是否显示小区列表
+    goSelectQuarters:function(){
+      let _this = this;
+      _this.isSelectQuarters = true;
+      // 获取小区列表
+      this.$http.post('/community/getDistributionCommunityList', {
+      },{
+        "encryptType":1
+      }).then(function(res){
+        if(res.resultCode != 0){
+          alert(res.msg);
+          return false;
+        }
+        _this.quartersLists = res.data;
+        //console.log(res.data);
+      }).catch(function(error) {
+        console.log(error)
+      });
 
+    },
+    // 选择小区
+    selectCurrentQuarters:function(name,id){
+      this.distributionCommunityId = id;
+      this.distributionCommunityName = name;
+      this.isSelectQuarters = false;
+    },
+    // 新增地址
+    add:function(){
+      let _this = this;
+      // 默认地址
+      _this.isDefault ? _this.isDefault = 1 : _this.isDefault = 0;
+      // 提交数据
+      this.$http.post('/community/addReceiptAddress', {
+        'name':_this.name,
+        'mobile':_this.mobile,
+        "distributionCommunityId": _this.distributionCommunityId,
+        'address':_this.address,
+        'isDefault':_this.isDefault
+      },{
+        "encryptType":1
+      }).then(function(res){
+        console.log(res);
+        if(res.resultCode != 0){
+          alert(res.msg);
+          return false;
+        }
+        _this.$refs.modalToast.is = false;
+        _this.$router.back();
+      }).catch(function(error) {
+        console.log(error)
+      })
+    },
+    // 修改地址
+    modify:function(){
+      let _this = this;
+      // 默认地址
+      _this.isDefault ? _this.isDefault = 1 : _this.isDefault = 0;
+      // 修改地址数据
+      this.$http.post('/community/modifyReceiptAddress', {
+        'addressId':this.$route.query.addressId,
+        'name':_this.name,
+        'mobile':_this.mobile,
+        "distributionCommunityId": _this.distributionCommunityId,
+        'address':_this.address,
+        'isDefault':_this.isDefault
+      },{
+        "encryptType":1
+      }).then(function(res){
+        //console.log(res);
+        if(res.resultCode != 0){
+          alert(res.msg);
+          return false;
+        }
+        _this.$refs.modalToast.is = false;
+        _this.$router.back();
+      }).catch(function(error) {
+        console.log(error)
+      })
+    },
+    // 地址提交
+    save:function(){
+      let _this = this;
+      // 验证
+
+
+      // 显示加载中
+      _this.$refs.modalToast.is = true;
+      // 提交
+      if(this.path == '/userAddress/modify'){
+        // 修改地址
+        _this.modify();
+      }else if(this.path == '/userAddress/add'){
+        // 增加地址
+        _this.add();
+      }
+
+    }
   },
   components: {
-
+    modalToast
   }
 }
 </script>
--- src/views/userAddress/userAddress.vue
+++ src/views/userAddress/userAddress.vue
@@ -1,61 +1,106 @@
 <template>
+<div class="view">
 
   <div class="wrap">
 
     <article class="main">
       <div class="mainScroll userAddress">
-        <div class="title">
+        <div class="title" v-if="canDeliverAddressInfos.length > 0">
           可配送地址
         </div>
-        <ul>
-          <li>
-            <div><span class="name">姓名</span><span class="tel">130000000000</span></div>
-            <p>地址地址地址地址地址地址地址地址地址地址地址地址地址地址</p>
-            <i></i>
-          </li>
-          <li>
-            <div><span class="name">姓名</span><span class="tel">130000000000</span></div>
-            <p>地址地址地址地址地址地址地址地址地址地址地址地址地址地址</p>
-            <i></i>
+        <ul v-if="canDeliverAddressInfos.length > 0">
+          <li v-for="(list,index) in canDeliverAddressInfos">
+            <router-link :to="{path:'/userAddress/modify',query: { index: index,communityId: list.communityId,addressId:list.addressId }}">
+              <div><span class="name">{{list.name}}</span><span class="tel">{{list.mobile}}</span></div>
+              <p>{{list.communityName}} | {{list.address}}</p>
+              <i v-if="list.isAuthenAddress"></i>
+            </router-link>
           </li>
         </ul>
-        <div class="title">
+        <div class="title" v-if="noDeliverAddressInfos.length > 0">
           不可配送地址
         </div>
-        <ul>
-          <li class="select">
-            <div><span class="name">姓名</span><span class="tel">130000000000</span></div>
-            <p>地址地址地址地址地址地址地址地址地址地址地址地址地址地址</p>
-            <i></i>
+        <ul v-if="noDeliverAddressInfos.length > 0">
+          <li class="select" v-for="(list,index) in noDeliverAddressInfos">
+            <a href="javascript:;">
+              <div><span class="name">{{list.name}}</span><span class="tel">{{list.mobile}}</span></div>
+              <p>{{list.communityName}} | {{list.address}}</p>
+              <i v-if="list.isAuthenAddress"></i>
+            </a>
           </li>
         </ul>
       </div>
+
     </article>
     <footer>
       <div class="userAddressBtn">
-        <div>提交</div>
+        <router-link to="/userAddress/add">
+          新增地址
+        </router-link>
       </div>
     </footer>
   </div>
 
+  <transition name="SlideRightLeft">
+    <router-view :list="canDeliverAddressInfos"></router-view>
+  </transition>
+
+  <modal-toast ref="modalToast" :txt="'加载中'" :icon="'loading'" :time="0"></modal-toast>
+
+</div>
 </template>
 <script>
+import Vue from 'vue'
+import simplestorage from 'simplestorage.js'
+import notData from '../common/notData.vue'
+import modalToast from '../common/modalToast.vue'
 
 export default {
   name: 'userAddress',
   data() {
     return{
-
+      canDeliverAddressInfos:'',
+      noDeliverAddressInfos:''
     }
   },
   mounted() {
 
+    this.init();
+
   },
   methods: {
-
+    init(){
+      let _this = this;
+      // 显示加载中
+      _this.$refs.modalToast.is = true;
+      this.$http.post('/community/getStoreDeliveryAddress', {
+        "distributionCommunityId": simplestorage.get('HLXK_DISTRIBUTION').id
+      },{
+        "encryptType":1
+      }).then(function(res){
+        //console.log(JSON.stringify(res.data));
+        if(res.resultCode != 0){
+          alert(res.msg);
+          return false;
+        }
+        _this.canDeliverAddressInfos = res.data.canDeliverAddressInfos;
+        _this.noDeliverAddressInfos = res.data.noDeliverAddressInfos;
+        // 隐藏加载中
+        _this.$refs.modalToast.is = false;
+      }).catch(function(error) {
+        console.log(error)
+      })
+    }
   },
   components: {
-
+    notData,
+    modalToast
+  },
+  beforeRouteUpdate(to, from, next){
+    if(to.name == 'userAddress'){
+      this.init();
+    }
+    next();
   }
 }
 </script>
