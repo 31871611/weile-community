2706655db40118635144b1368e47583a09f37130
btname <btname@163.com>
2017-04-25 14:04:57 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import simplestorage from 'simplestorage.js'
 import Router from 'vue-router'
+import fetch from '../utils/fetch'
 import Home from '@/views/index/index'
 
 Vue.use(Router)
@@ -15,66 +16,85 @@ const router = new Router({
       name: 'home',
       component: Home
     }, {
+      // 选择小区
       path: '/selectQuarters',
       name: 'selectQuarters',
       component: resolve => require(['@/views/index/selectQuarters'], resolve)
     }, {
+      // 社区
       path: '/community',
       name: 'community',
       component: resolve => require(['@/views/community/community'], resolve)
     }, {
+      // 限时抢购
       path: '/sale',
       name: 'sale',
       component: resolve => require(['@/views/list/sale'], resolve)
     }, {
+      // 专题列表
       path: '/subject',
       name: 'subject',
       component: resolve => require(['@/views/list/subject'], resolve)
     }, {
+      // 领券专题
       path: '/subjectCoupon',
       name: 'subjectCoupon',
       component: resolve => require(['@/views/list/subjectCoupon'], resolve)
     }, {
+      // 便利店
       path: '/store',
       name: 'store',
       component: resolve => require(['@/views/store/store'], resolve)
     }, {
+      // 购物车
       path: '/shopping',
       name: 'shopping',
       component: resolve => require(['@/views/shopping/shopping'], resolve)
     },{
+      // 商品详情页
       path: '/commodity',
       name: 'commodity',
       component: resolve => require(['@/views/commodity/commodity'], resolve)
     },{
+      // 结算
       path: '/payorder',
       name: 'payorder',
       component: resolve => require(['@/views/payorder/payorder'], resolve)
     },{
+      // 支付结束
       path: '/payResult',
       name: 'payResult',
       component: resolve => require(['@/views/payResult/payResult'], resolve)
     },{
+      // 登录
       path: '/login',
       name: 'login',
       component: resolve => require(['@/views/login/login'], resolve)
     },{
+      // 用户中心
       path: '/userHome',
       name: 'userHome',
-      component: resolve => require(['@/views/userHome/userHome'], resolve)
+      component: resolve => require(['@/views/userHome/userHome'], resolve),
+      meta:{
+        requireAuth: true
+      }
     },{
+      // 我的订单
       path: '/userOrder',
       name: 'userOrder',
       component: resolve => require(['@/views/userOrder/userOrder'], resolve)
     },{
+      // 我的订单详情
       path: '/userOrderDetails',
       name: 'userOrderDetails',
       component: resolve => require(['@/views/userOrderDetails/userOrderDetails'], resolve)
     },{
+      // 申请退货
       path: '/userApplyBack',
       name: 'userApplyBack',
       component: resolve => require(['@/views/userApplyBack/userApplyBack'], resolve)
     },{
+      // 我的地址
       path: '/userAddress',
       name: 'userAddress',
       component: resolve => require(['@/views/userAddress/userAddress'], resolve),
@@ -89,10 +109,12 @@ const router = new Router({
         }
       ]
     },{
+      // 我的优惠券
       path: '/userCoupon',
       name: 'userCoupon',
       component: resolve => require(['@/views/userCoupon/userCoupon'], resolve)
     },{
+      // 优惠券详情
       path: '/couponDetails',
       name: 'couponDetails',
       component: resolve => require(['@/views/userCoupon/couponDetails'], resolve)
@@ -113,25 +135,45 @@ const router = new Router({
 **/
 router.beforeEach((to, from, next) => {
   //console.log(to);
-  console.log(simplestorage.get('HLXK_SESSION'));
-  console.log(localStorage.getItem('simpleStorage'));
-  if(localStorage.getItem('HLXK_DISTRIBUTION') || to.path == '/selectQuarters'){
-
-
-
-
-
-    //if(to.meta.requireAuth){
-    //
-    //}else{
-    //  next()
-    //}
-    next();
+  if(!simplestorage.get('HLXK_SESSION')){
+    // 以游客方式登录
+    fetch.post('/community/touristLogin').then(function(res) {
+      //console.log(res)
+      let data = res.data || {};
+      if(res.resultCode == 0){
+        simplestorage.set('HLXK_SESSION', data.session);
+        simplestorage.set('HLXK_KEY', data.key);
+        go();
+      }
+    }).catch(function(error) {
+      console.log(error)
+    })
   }else{
-    // 去选择小区
-    console.log('去选择小区');
-    next({ path: '/selectQuarters' });
+    go();
   }
+
+  function go(){
+    if(simplestorage.get('HLXK_DISTRIBUTION') || to.path == '/selectQuarters'){
+      // 是否需要登录
+      if(to.meta.requireAuth){
+        if (simplestorage.get('HLXK_STATUS')) {
+          next();
+        }else {
+          next({
+            path: '/login',
+            query: {url: to.fullPath}
+          })
+        }
+      }else{
+        next()
+      }
+    }else{
+      console.log(to.path);
+      // 去选择小区
+      next({ path: '/selectQuarters',query: { url: to.fullPath }});
+    }
+  }
+
 })
 
 export default router;
