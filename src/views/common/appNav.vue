ae2674dbe8aa33c7ea58c340281fedec2b678fc4
btname <btname@163.com>
2017-05-02 16:56:13 +0800

    底部导航

--- src/views/common/appNav.vue
+++ src/views/common/appNav.vue
@@ -4,7 +4,9 @@
     <router-link to="/" class="home" :class="{select : selectClass == 'home'}">首页</router-link>
     <router-link to="community" class="community" :class="{select : selectClass == 'community'}">社区</router-link>
     <router-link to="store" class="convenient" :class="{select : selectClass == 'store'}">便利店</router-link>
-    <router-link to="shopping" class="shopping" :class="{select : selectClass == 'shopping'}">购物车</router-link>
+    <router-link to="shopping" class="shopping" :class="{select : selectClass == 'shopping'}">
+      购物车<b v-if="shoppingNum > 0">{{shoppingNum}}</b>
+    </router-link>
     <router-link to="userHome" class="my" :class="{select : selectClass == 'userHome'}">我的</router-link>
   </div>
 
@@ -13,7 +15,16 @@
 
 export default {
   name: 'appNav',
-  props:['selectClass'],
+  props:{
+    selectClass:{
+      type:String
+    },
+    shoppingNum:{
+      type:Number,
+      default: 0
+    }
+  },
+  //props:['selectClass'],
   data() {
     return{
 
