d0ffffceaafdbf1ed8ddd850895d1bbcdf8fa3ee
31871611 <31871611@qq.com>
2017-05-14 17:16:50 +0800

    toast

--- src/views/common/modalToast.vue
+++ src/views/common/modalToast.vue
@@ -10,42 +10,56 @@
 </template>
 <script>
 /*
-  icon：成功success、错误error、警告warning、通知info、加载中loading
-  time：显示秒数，0一直运行
-  <modal-toast :icon="'error'" :txt="'标题'" :time="10" ref="modalToast"></modal-toast>
-  this.$refs.modalToast.is = true;
 
   文字提示完还需要有个回调
-*/
+
+import toast from '../common/modalToast.vue'
+<toast ref="toast"></toast>
+
+this.$refs.toast.toast({
+  txt:'提示',
+  icon:'loading',           // icon：成功success、错误error、警告warning、通知info、加载中loading
+  time:0                    // time：显示秒数，0一直运行
+});
+
+// 关闭
+this.$refs.toast.is = false;
+
+ */
 export default {
-  props:{
-    icon:String,
-    txt:{
-      type:String,
-      default:'提示文字'
-    },
-    time:{
-      type:Number,
-      default: 3
-    }
-  },
   data(){
     return{
-      is:false
+      is:false,
+      icon: '',
+      txt: '提示文字',
+      time: 3
     }
   },
-  beforeUpdate() {
-    let _this = this;
-    let t = null;
-
-    if(_this.time !== 0){
+  methods:{
+    toast(setting,callback){
+      let _this = this;
+      this.icon = setting.icon ||  false;
+      this.txt = setting.txt || false;
+      this.is = true;
+      let time;
+      if(setting.time === 0){
+        // 0，一直在
+        return false;
+      }
+      if(setting.time == undefined){
+        // undefined ，就是没传，3秒关
+        time = this.time;
+      }else{
+        // 有传按，传的时间关闭
+        time = setting.time;
+      }
+      let t = null;
       // 多少秒后关闭
       t = setTimeout(function(){
         _this.is = false;
         clearTimeout(t);
-      },this.time * 1000)
+      },time * 1000)
     }
-
   }
 }
 </script>
