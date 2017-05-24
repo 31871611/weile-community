39c0a17adfe00edf33f6c072a785c7fc15a8f6f5
btname <btname@163.com>
2017-05-24 19:23:26 +0800

    登录

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -9,14 +9,14 @@
       <input type="tel" placeholder="请输入手机号码" autofocus v-model="telephone" />
     </div>
     <div class="itemInfo">
-      <i class="password"></i>
-      <input type="password" placeholder="请输入密码(不少于6位数)" v-model="password" />
+      <i class="code"></i>
+      <input type="text" placeholder="请输入验证码" v-model="code" />
+      <span class="getCode" @click="getCode()" :class="{'select':isCode}">{{codeTxt}}</span>
     </div>
-    <div class="itemInfo" v-if="false">
-      <i class="mail"></i>
-      <input type="password" placeholder="验证码" />
-      <span class="getCode">获取验证码</span>
-    </div>
+    <!--<div class="itemInfo">-->
+      <!--<i class="password"></i>-->
+      <!--<input type="password" placeholder="请输入验证码" v-model="password" />-->
+    <!--</div>-->
 
     <div class="loginBtn" @click="submit">
       登录
@@ -35,12 +35,16 @@ import modalToast from '../common/modalToast.vue'
 import cart from '../../plugins/cart'
 import appNav from '../common/appNav.vue';
 
+
 export default {
   name: 'login',
   data() {
     return {
       telephone:'',
       password:'',
+      code:'',
+      codeTxt:'获取验证码',
+      isCode:false,
       url:''
     }
   },
@@ -61,6 +65,66 @@ export default {
     }
   },
   methods: {
+    // 获取验证码
+    getCode:function(){
+      let _this = this;
+      let num = 60;
+      let t = null;
+
+      if(_this.isCode){
+        //console.log('不可点');
+        return false;
+      }
+
+      if(_this.telephone == '' || _this.telephone.length != 11 || !/^(13|14|15|17|18)\d{9}$/.test(_this.telephone)){
+        _this.$refs.modalToast.toast({
+          txt:'请输入正确的手机号码'
+        });
+        return false;
+      }
+
+      _this.codeTxt = num + 's';
+      _this.isCode = true;
+      // 请求验证码
+      this.$http.post('/community/getVerificationCode',{
+        "telephone":_this.telephone,
+        "projectId":simplestorage.get('projectId'),
+        "distributionCommunityId":simplestorage.get('HLXK_DISTRIBUTION').id,
+        "openid":simplestorage.get('HLXK_OPENID')
+      },{
+        "encryptType":0
+      }).then(function(res) {
+        //console.log(res)
+        if(res.resultCode == 0){
+          _this.$refs.modalToast.toast({
+            txt:res.msg
+          });
+        }else{
+          // 错误提示
+          _this.$refs.modalToast.toast({
+            txt:res.msg
+          });
+          return false;
+        }
+
+      }).catch(function(error) {
+        console.log(error)
+      })
+
+      // 倒记时
+      t = setInterval(function(){
+        if(num == 0){
+          _this.codeTxt = '获取验证码';
+          _this.isCode = false;
+          clearTimeout(t);
+        }else{
+          num--;
+          _this.codeTxt = num + 's';
+        }
+      },1000)
+
+    },
+    // 登录
     submit:function(){
       let _this = this;
       if(_this.telephone == '' || _this.telephone.length != 11 || !/^(13|14|15|17|18)\d{9}$/.test(_this.telephone)){
@@ -69,12 +133,18 @@ export default {
         });
         return false;
       }
-      if(_this.password.length < 6){
+      if(_this.code == ''){
         _this.$refs.modalToast.toast({
-          txt:'请输入不少于6位数的密码'
+          txt:'请输入正确的验证码'
         });
         return false;
       }
+//      if(_this.password.length < 6){
+//        _this.$refs.modalToast.toast({
+//          txt:'请输入不少于6位数的密码'
+//        });
+//        return false;
+//      }
       // 显示加载中
       _this.$refs.modalToast.toast({
         txt:'加载中',
@@ -82,22 +152,23 @@ export default {
         time:0
       });
 
-      this.$http.post('/community/login',{
+      this.$http.post('/community/checkVerificationCode',{
         "telephone":_this.telephone,
-        "password":cryptoUtils.md5(_this.password),
+        "verifyCode":_this.code,
         "projectId":simplestorage.get('projectId'),
         "distributionCommunityId":simplestorage.get('HLXK_DISTRIBUTION').id,
-        //"openid":simplestorage.get('HLXK_OPENID')
+        "openid":simplestorage.get('HLXK_OPENID'),
+        "sessionId":simplestorage.get('HLXK_SessionId')
       },{
         "encryptType":0
       }).then(function(res) {
-        //console.log(res)
+        console.log(res)
         let data = res.data || {}
         if(res.resultCode == 0){
           simplestorage.set('HLXK_SESSION', data.session);
           simplestorage.set('HLXK_KEY', data.key);
           // 保存用户信息
-          simplestorage.set('HLXK_UserInfo', data.userInfo);
+          //simplestorage.set('HLXK_UserInfo', data.userInfo);
           // 是否登录状态
           simplestorage.set('HLXK_STATUS', true);
           // 保存登录时间
